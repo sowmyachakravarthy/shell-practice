db6b6aeb54c6ccdb516739ac7a508b2949a2558e
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-18 16:31:29 +0530

    conversation

--- 04-argument.sh
+++ 04-argument.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$1=value1
-$2=value2
+PERSON1=$1
+PERSON2=$2
 
-echo " $value1 : i'm still trying how to learn scripting"
-echo " $value2 : don't worry, slowly you will understand"
-echo " $value1 : But, i will be behind if i don't understand"
-echo " $value2 : No worries, everybody will have there own pace"
-echo " $value1 : so is it all fine ? "
-echo " $value2 : Yes, just keep practicing. All the best"
+echo " $PERSON1 : i'm still trying how to learn scripting"
+echo " $PERSON2 : don't worry, slowly you will understand"
+echo " $PERSON1 : But, i will be behind if i don't understand"
+echo " $PERSON2 : No worries, everybody will have there own pace"
+echo " $PERSON1 : so is it all fine ? "
+echo " $PERSON2 : Yes, just keep practicing. All the best"
