3386fe15a1e41d08504d65ebba5c89d7410184d6
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-18 16:29:49 +0530

    conversation

---
+++ 04-argument.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+$1=value1
+$2=value2
+
+echo " $value1 : i'm still trying how to learn scripting"
+echo " $value2 : don't worry, slowly you will understand"
+echo " $value1 : But, i will be behind if i don't understand"
+echo " $value2 : No worries, everybody will have there own pace"
+echo " $value1 : so is it all fine ? "
+echo " $value2 : Yes, just keep practicing. All the best"
