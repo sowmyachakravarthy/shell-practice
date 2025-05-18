232c077a004e5fcc96c030d7091ac30066ea6e05
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-18 17:00:03 +0530

    conversation

---
+++ 07-array.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#array is like masala petta, okate dantlo multiple values store cheskochu, oke variable lo we can
+#store multiple values.
+
+#ila oka movie ane variable lo we can put multiple values
+MOVIES=("court" "Subham" "Hi nanna" "Ante sundaraniki")
+
+#array lo index starts from 0, court=0, subham=1, hi nanna=3... ala
+
+echo "First movie: ${MOVIES[0]}" #to execute court, flower brackets pedthe 0 ni index input laga teeskuntundi
+#lekapothe it prints 0 in the output
+
+echo "All movies: ${MOVIES[@]}" #ikada to print all movies we use @ = all
+
+
+
