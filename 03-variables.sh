5d7ed208e6173edf6538b10a1e162057e0bcbca5
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-18 15:42:39 +0530

    conversation

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+$PERSON1 = Sowmya
+$PERSON2 = Shiva
+
+echo " $PERSON1 : i'm still trying how to learn scripting"
+echo " $PERSON2 : don't worry, slowly you will understand"
+echo " $PERSON1 : But, i will be behind if i don't understand"
+echo " $PERSON2 : No worries, everybody will have there own pace"
+echo " $PERSON1 : so is it all fine ? "
+echo " $PERSON2 : Yes, just keep practicing. All the best"
