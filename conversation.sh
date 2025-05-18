3380b8de501fc6a1b8bff3f44213f927199bbc8a
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-18 15:31:34 +0530

    conversation

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo " sowmya : i'm still trying how to learn scripting"
+echo " Siva : don't worry, slowly you will understand"
+echo " sowmya : But, i will be behind if i don't understand"
+echo " Siva : No worries, everybody will have there own pace"
+echo " so is it all fine ? "
+echo " Yes, just keep practicing. All the best"
