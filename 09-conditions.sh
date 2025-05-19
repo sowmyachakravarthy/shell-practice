59cd1c557eb757121cf07eafc3de1b4b3068ac01
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-19 12:58:08 +0530

    conversation

---
+++ 09-conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 50000 -lt 80000 ]
+
+    echo "you can send only 5000 to your parents"
+
+elif [ $NUMBER -gt 80000 ] 
+then
+    echo "you can now send 10000 to your parents"
+
+else
+    echo " don't send money to your parents, we can't afford now "
+
+fi
