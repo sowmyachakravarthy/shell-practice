6085bc51ee4d2f21310e49f4e6beb69d8590b6ca
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-21 12:30:33 +0530

    conversation

--- 13-logs.sh
+++ 13-logs.sh
@@ -14,10 +14,51 @@ echo "script started executing at: $(date)"
 
 echo "Test line" >>$LOG_FILE
 
+
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
     exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access"
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it" | tee -a $LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "MySQL"
+else
+    echo -e "Nothing to do MySQL... $Y already installed $N" | tee -a $LOG_FILE
+fi
+
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it" | tee -a $LOG_FILE
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "python3"
+else
+    echo -e "Nothing to do python... $Y already installed $N" | tee -a $LOG_FILE
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it" | tee -a $LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "nginx"
+else
+    echo -e "Nothing to do nginx... $Y already installed $N" | tee -a $LOG_FILE
 fi
