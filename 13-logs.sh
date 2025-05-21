061e79f540e54f7b18b0e37ee46281a804509341
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-21 11:53:21 +0530

    conversation

---
+++ 13-logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
