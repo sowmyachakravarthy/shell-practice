4ac302bb644a8bac7bb9398a08d70f89d0c7a1d8
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-21 11:57:38 +0530

    conversation

--- 13-logs.sh
+++ 13-logs.sh
@@ -8,3 +8,14 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir $LOGS_FOLDER
+echo "script started executing at: $(date)"
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access"
+fi
