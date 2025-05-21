0543ef6139e36e5f2684e41f18978c3330fb3854
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-21 12:50:07 +0530

    conversation

--- 15-loops.sh
+++ 15-loops.sh
@@ -44,25 +44,5 @@ do
         VALIDATE $? "$package"
     else
     echo -e "Nothing to do $package... $Y already installed $N" | tee -a $LOG_FILE
-fi
-
-
-# dnf list installed python3 &>>$LOG_FILE
-# if [ $? -ne 0 ]
-# then
-#     echo "python3 is not installed... going to install it" | tee -a $LOG_FILE
-#     dnf install python3 -y &>>$LOG_FILE
-#     VALIDATE $? "python3"
-# else
-#     echo -e "Nothing to do python... $Y already installed $N" | tee -a $LOG_FILE
-# fi
-
-# dnf list installed nginx &>>$LOG_FILE
-# if [ $? -ne 0 ]
-# then
-#     echo "nginx is not installed... going to install it" | tee -a $LOG_FILE
-#     dnf install nginx -y &>>$LOG_FILE
-#     VALIDATE $? "nginx"
-# else
-#     echo -e "Nothing to do nginx... $Y already installed $N" | tee -a $LOG_FILE
-# fi
+    fi
+done    
