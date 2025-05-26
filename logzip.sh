a36fbd7cc92b6f13c252ae886022741c08a6a43e
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-26 20:02:57 +0530

    conversation

--- logzip.sh
+++ logzip.sh
@@ -21,20 +21,20 @@ fi
 
 
 cp $SOURCE_DIR/messages $DEST_DIR &>>$LOG_FILE
-VALIDATE $? "Copying messages to $DEST_DIR"
+
 
 >$SOURCE_DIR/messages &>>$LOG_FILE
-VALIDATE $? "Nullifying messages"
+
 
 cd $DEST_DIR &>>$LOG_FILE
-VALIDATE $? "Going to $DEST_DIR"
+
 
 gzip messages &>>$LOG_FILE
-VALIDATE $? "Zipping Messages"
+
 
 mv messages.gz messages.$DATE.gz &>>$LOG_FILE
-VALIDATE $? "Renaming the file"
+
 
 mv messages.$DATE.gz $SOURCE_DIR &>>$LOG_FILE
-VALIDATE $? "Placing the zip file in $SOURCE_DIR"
+
 
